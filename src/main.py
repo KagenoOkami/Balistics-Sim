7c8feda62139ee9d93a230e309e1aee7d6433621
Unknown <Seseellybon@gmail.com>
2018-11-15 19:11:58 +0100

    Fixed subplots
    Also added 3d plot

--- src/main.py
+++ src/main.py
@@ -3,11 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+from mpl_toolkits.mplot3d import Axes3D
+
+
+
 
 
 Timeframe = 0.033 # second
 
-wind = (0, 0)
+wind = (5, 0)
 gravity_c = 9
 
 drag_y = 0.999
@@ -15,13 +19,13 @@ drag_x = 0.999
 drag_z = 0.999
 
 
-def shoot( angle = 0, rotation = 0 propelant = 0):
+def shoot( angle = 0, rotation = 0, propelant = 0):
 
 	angle = math.radians(angle)
 
-	points_x = [1]
-	points_y = [1]
-	points_z = [1]
+	points_x = [1] # West
+	points_y = [1] # Up
+	points_z = [1] # North
 
 	velocity_x = propelant*10* math.cos(angle)
 	velocity_y = propelant*10* math.sin(angle)
@@ -52,7 +56,7 @@ def shoot( angle = 0, rotation = 0 propelant = 0):
 	
 
 	print("A shell hit the ground", str(int(projectile_x)), "Meters away" )
-	return pd.DataFrame({'x': points_x, 'y': points_y }), pd.DataFrame({'z': points_z, 'y': points_y })
+	return (points_x, points_y, points_z )
 
 
 
@@ -62,22 +66,47 @@ print("start sim")
 # Using subplots, try to add a second axis, making a 3d plot of sorts.
 # Maybe
 
-df= shoot( 45, 8 )
-plt.subplot(1, 2, 1)
-plt.plot( 'x', 'y', data=df[0], marker='.', markersize=1, linewidth=1.9)
-plt.subplot(1, 2, 2)
-plt.plot( 'z', 'y', data=df[1], marker='.', markersize=1, linewidth=1.9)
+points = shoot( 45, 45, 10 )
 
+points_x = points[0]
+points_y = points[1]
+points_z = points[2]
+
+df_North = pd.DataFrame( points_x, points_y )
+df_West  = pd.DataFrame( points_z, points_y )
+df_Top   = pd.DataFrame( points_x, points_z )
+
+plt.subplot(3, 2, 1)
+plt.plot( points_x, points_y, marker='.', markersize=1, linewidth=1.9)
+plt.title('Shot 1')
+plt.ylabel('Height (m)')
+plt.xlabel('West (m)')
+
+plt.subplot(3, 2, 3)
+plt.plot( points_z, points_y, marker='.', markersize=1, linewidth=1.9)
+plt.ylabel('Height (m)')
+plt.xlabel('North (m)')
+
+plt.subplot(3, 2, 5)
+plt.plot( points_x, points_z, marker='.', markersize=1, linewidth=1.9)
+plt.ylabel('North (m)')
+plt.xlabel('Top (m)')
+
+
+plt.subplot(3, 2, 2, projection='3d')
+plt.plot( points_x, points_z, points_y, marker='.', markersize=1, linewidth=1.9)
+plt.ylabel('Height (m)')
+plt.xlabel('West (m)')
 '''
-df= shoot( 30, 8 )
-plt.subplot(3, 1, 3)
-plt.plot( 'x', 'y', data=df, marker='.', markersize=1, linewidth=1.9)
-
+plt.subplot(1, 1, 1)
+plt.plot( 'x', 'y',  data=df_Top, marker='.', markersize=1, linewidth=1.9)
+plt.ylabel('North (m)')
+plt.xlabel('West (m)')
 '''
-
 
 
 print("end sim")
+
 
 plt.show()
 
