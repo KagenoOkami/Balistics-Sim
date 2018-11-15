132e28e56c73efa310ce1bbbced707a7668f650b
Unknown <Seseellybon@gmail.com>
2018-11-15 20:26:56 +0100

    Added Range Table, neated main.py

--- src/main.py
+++ src/main.py
@@ -9,9 +9,9 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 
-Timeframe = 0.033 # second
+Timeframe = 0.016 # second
 
-wind = (5, 0)
+wind = (0, 0)
 gravity_c = 9
 
 drag_y = 0.999
@@ -21,14 +21,14 @@ drag_z = 0.999
 
 def shoot( angle = 0, rotation = 0, propelant = 0):
 
-	angle = math.radians(angle)
+	angle_r = math.radians(angle)
 
 	points_x = [1] # West
 	points_y = [1] # Up
 	points_z = [1] # North
 
-	velocity_x = propelant*10* math.cos(angle)
-	velocity_y = propelant*10* math.sin(angle)
+	velocity_x = propelant*10* math.cos(angle_r)
+	velocity_y = propelant*10* math.sin(angle_r)
 	velocity_z = propelant*10* math.sin(rotation)
 
 	projectile_x = 0
@@ -54,8 +54,10 @@ def shoot( angle = 0, rotation = 0, propelant = 0):
 		points_y.append(int(projectile_y))
 		points_z.append(int(projectile_z))
 	
+#	print("A shell hit the ground", np.linalg.norm([projectile_x, projectile_z]), "Meters away" )
+#	print("At", [projectile_x,projectile_y, projectile_z] )
+	print( angle, '\t', propelant, '\t', int( np.linalg.norm([projectile_x, projectile_z]) ) )
 
-	print("A shell hit the ground", str(int(projectile_x)), "Meters away" )
 	return (points_x, points_y, points_z )
 
 
@@ -63,10 +65,20 @@ def shoot( angle = 0, rotation = 0, propelant = 0):
 print("start sim")
  
 
-# Using subplots, try to add a second axis, making a 3d plot of sorts.
-# Maybe
+# Piece of script used to generate the Range Table
 
-points = shoot( 45, 45, 10 )
+'''
+for a in range(10, 100, 10):
+	for p in range(1, 11, 1):
+		shoot( a, 0, p )
+
+print("end sim")
+'''
+
+# Plot DataFrame setup
+# Didn't use a oneliner for simplicity
+
+points = shoot( 40, 0, 5)
 
 points_x = points[0]
 points_y = points[1]
@@ -76,36 +88,35 @@ df_North = pd.DataFrame( points_x, points_y )
 df_West  = pd.DataFrame( points_z, points_y )
 df_Top   = pd.DataFrame( points_x, points_z )
 
-plt.subplot(3, 2, 1)
+# Plot as viewed from the South
+plt.subplot(3, 1, 1)
 plt.plot( points_x, points_y, marker='.', markersize=1, linewidth=1.9)
 plt.title('Shot 1')
 plt.ylabel('Height (m)')
 plt.xlabel('West (m)')
 
-plt.subplot(3, 2, 3)
+# Plot as viewed from the East
+plt.subplot(3, 1, 2)
 plt.plot( points_z, points_y, marker='.', markersize=1, linewidth=1.9)
 plt.ylabel('Height (m)')
 plt.xlabel('North (m)')
 
-plt.subplot(3, 2, 5)
+# Plot as viewed from the Top
+plt.subplot(3, 1, 3)
 plt.plot( points_x, points_z, marker='.', markersize=1, linewidth=1.9)
 plt.ylabel('North (m)')
 plt.xlabel('Top (m)')
 
 
-plt.subplot(3, 2, 2, projection='3d')
-plt.plot( points_x, points_z, points_y, marker='.', markersize=1, linewidth=1.9)
-plt.ylabel('Height (m)')
-plt.xlabel('West (m)')
-'''
-plt.subplot(1, 1, 1)
-plt.plot( 'x', 'y',  data=df_Top, marker='.', markersize=1, linewidth=1.9)
-plt.ylabel('North (m)')
-plt.xlabel('West (m)')
-'''
+# 3D plot
+fig1 = plt.figure(figsize=plt.figaspect(0.5))
+
+ax = plt.subplot(1, 1, 1, projection='3d')
+ax.plot( points_x, points_z, points_y, marker='.', markersize=1, linewidth=1.9)
 
 
-print("end sim")
+
+
 
 
 plt.show()
