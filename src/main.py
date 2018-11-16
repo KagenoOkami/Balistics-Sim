8f85c3bb14b20b7e25184758eea0eb358cf8d61d
Seseel Lybon <Seseellybon@gmail.com>
2018-11-16 14:10:45 +0100

    Made it a game
    Added a game loop. Generates a target and then has you shoot at it until you get within 5m of it.

--- src/main.py
+++ src/main.py
@@ -1,23 +1,37 @@
 import math
 import numpy as np
+from random import randint
 
 import Artillery
 
 
+# Generate a target
 
+target_x = randint(50,900)
+target_z = randint(50,900)
+
+print("Target is at x:"+ str(target_x), "y:"+ str( target_z ) )
+print("Hint: Distance is", int(np.hypot(target_x,target_z)), "meters")
 
 print("start sim")
 
 # Bit of code to ask user input. Doesn't play nice in Sublime, though
-'''
-angle = float( input("angle:") )
-rotation = float( input("rotation:") )
-propelant = float( input("propelant:") )
-'''
-
-points = Artillery.shoot( angle, rotation, propelant)
+hit = 0
+while hit == 0:
+	angle = float( input("angle:") )
+	rotation = float( input("rotation:") )
+	propelant = float( input("propelant:") )
 
 
+	points = Artillery.shoot( angle, rotation, propelant)
+
+	if points[0][-1] >= target_x-5 and points[0][-1] <= target_x+5:
+		if points[2][-1] >= target_z-5 and points[2][-1] <= target_z+5:
+			print("HIT")
+			hit = 1
+		else: print("Miss")
+	else:
+		print("Miss")
 
 
 # Piece of Script that visualises the trajectory
